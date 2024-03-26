488208d8adefc680a36ea0e7e60f985b189cee93
fdvodkax <149469082+fdvodkax@users.noreply.github.com>
2024-03-26 09:24:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,22 @@ def hello_world():
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value_kelvin = list_element.get('main', {}).get('temp')  # Température en Kelvin
+        temp_day_value_celsius = temp_day_value_kelvin - 273.15  # Conversion de Kelvin en Celsius
+        results.append({'Jour': dt_value, 'temp': temp_day_value_celsius})
+    return jsonify(results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
   
 if __name__ == "__main__":
   app.run(debug=True)
